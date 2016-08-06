9b3c26eedec5001ed9fe74a34322b8cb0ed4ae67
黎明 <suzw@59store.com>
2016-08-06 15:06:38 +0800

    aaaaaa

---
+++ linkUS/MEMsgModule/ViewModel/MEMsgViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  MEMsgViewModel.swift
+//  linkUS
+//
+//  Created by  黎明 on 16/8/6.
+//  Copyright © 2016年 sujeking. All rights reserved.
+//
+
+import UIKit
+
+class MEMsgViewModel: NSObject {
+
+}
