e30feacfafc5960931fd215cc58d69c8af654052
sujeking <sujeking@sujekingdemacbook-pro.local>
2016-08-11 00:31:40 +0800

    添加需求页面

---
+++ linkUS/MEHomeModule/Views/MENeedView.swift
@@ -0,0 +1,42 @@
+//
+//  MEServiceView.swift
+//  linkUS
+//
+//  Created by sujeking on 16/8/10.
+//  Copyright © 2016年 sujeking. All rights reserved.
+//
+
+import UIKit
+
+class MENeedView: UIView,UITableViewDataSource {
+    
+    var tableview: UITableView!
+    
+    override init(frame: CGRect) {
+        super.init(frame:frame)
+        self.tableview = UITableView(frame: self.bounds)
+        self.tableview.dataSource = self
+        self.addSubview(self.tableview)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let ides: String! = "ides"
+        
+        var cell = tableview.dequeueReusableCellWithIdentifier(ides)
+        if cell == nil {
+            cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: ides)
+        }
+        
+        cell?.textLabel?.text = "need \(indexPath.row)"
+        return cell!
+    }
+    
+}
